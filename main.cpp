a965c9426cf2ad7c4ad9437964fddbc6023e305e
原　崚河 <k022g0002@g.neec.ac.jp>
2024-02-11 16:48:54 +0900

    評価課題ok

--- main.cpp
+++ main.cpp
@@ -17,7 +17,8 @@
 #include "externals/imgui/imgui.h"
 #include "externals/imgui/imgui_impl_dx12.h"
 #include "externals/imgui/imgui_impl_win32.h"
-extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+extern IMGUI_IMPL_API LRESULT
+    ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 struct Vector4
 {
